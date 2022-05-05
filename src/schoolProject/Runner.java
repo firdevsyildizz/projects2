29a0caacbfdc3397f2a22db0a216ee757310476b
firdevs yildiz <firdevsomeryildiz@gmail.com>
2022-05-05 15:49:00 +0100

    05.05.22

---
+++ src/schoolProject/Runner.java
@@ -0,0 +1,12 @@
+package schoolProject;
+
+public class Runner {
+
+        public static void main(String[] args) throws InterruptedException {
+            Ogretmen.feykOgretmenEkle();
+            Depo.anaMenu();
+        }
+
+
+    }
+}
