f369efe448e82859d2c54c2c2028c19cf0dea613
firdevs yildiz <firdevsomeryildiz@gmail.com>
2022-05-09 20:45:31 +0100

    09.05.22

---
+++ src/javaBankAtm02/A01_UserScreen.java
@@ -0,0 +1,11 @@
+package javaBankAtm02;
+
+public class A01_UserScreen {
+
+    public static void main(String[] args) {
+        System.out.println("*************************************");
+        System.out.println("******** Welcome to JavaBank ********");
+        //atmStart();
+
+    }
+}
